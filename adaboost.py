bc5107127d1b8ec3ae2245edbe31f5e5cf9d2d6c
senghuot <senghuotlim@gmail.com>
2015-07-13 17:44:59 -0700

    whasdf

--- adaboost.py
+++ adaboost.py
@@ -1,16 +1,16 @@
 from numpy import *
 from sklearn import tree
 
-ITERATIONS = 2
+ITERATIONS = 50
 
-# data is already encoding with excel. This is just a quick test for adaboost.
+# this algorithm follows directly from class
 def main():
-  clf = tree.DecisionTreeClassifier(max_depth=2, criterion='entropy')
+  clf = tree.DecisionTreeClassifier(max_depth=1, criterion='entropy')
   train_tmp = genfromtxt('data/data-filtered-binary-number.txt')
 
 
   # setting up hyper parameters
-  TRAIN_LEN = int(len(train_tmp) * (1.0/10))
+  TRAIN_LEN = int(len(train_tmp) * (10/100.0))
 
   # training weights
   d = [1.0/TRAIN_LEN] * TRAIN_LEN
@@ -21,10 +21,7 @@ def main():
 
   for i in range(0, TRAIN_LEN):
     tmp = train_tmp[i]
-    x = []
-    for j in range(0, len(tmp)-2):
-      x.append(tmp[j])
-    train_x.append(x)
+    train_x.append(tmp[0:len(tmp)-1])
     train_y.append(tmp[len(tmp)-1])
 
   # Testing set
@@ -33,12 +30,8 @@ def main():
 
   for i in range(TRAIN_LEN, len(train_tmp)):
     tmp = train_tmp[i]
-    x = []
-    for j in range(0, len(tmp)-2):
-      x.append(tmp[j])
-    test_x.append(x)
+    test_x.append(tmp[0:len(tmp)-1])
     test_y.append(tmp[len(tmp)-1])
-
 
   # Convert narrays to matrices
   train_x = matrix(train_x)
@@ -48,6 +41,7 @@ def main():
 
   # Accumulation of Ys
   final_y = [0] * len(test_y)
+
   for iteration in range(0, ITERATIONS): 
     # Checking for error rate for training data
     clf = clf.fit(train_x, train_y, sample_weight=d)
@@ -55,34 +49,33 @@ def main():
 
     # Getting epsilon
     epsilon = 0
-    toal_weight = 0
     for i in range(0, len(predicted_y)):
       epsilon += d[i] * pi(predicted_y[i], train_y[i])
-      toal_weight += d[i]
-    epsilon /= toal_weight
+
+
 
     alpha = 1
     if epsilon != 0:
       # Updates and normalizing the weights
-      alpha = math.log((1-epsilon)/epsilon)
-      total_weight = 0
+      alpha = 0.5 * math.log((1-epsilon)/epsilon)
+      total_weight_new = 0
       for i in range(0, len(predicted_y)):
-        power = alpha * pi(train_y[i], predicted_y[i])
+        power = -1 * alpha * train_y[i] * predicted_y[i]
         d[i] = d[i] * math.pow(math.e, power)
-        total_weight += d[i]
-      
+        total_weight_new += d[i]
+
       # Normalizing
       for i in range(0, len(predicted_y)):
-        d[i] = d[i] / total_weight
+        d[i] = d[i] / total_weight_new
 
     # Accumulate voting on training data
-    error = 0
+    correct = 0
     final_y += alpha * clf.predict(test_x)
     for i in range(0, len(final_y)):
-      if sign(final_y[i]) != test_y[i]:
-        error += 1 
+      if sign(final_y[i]) == test_y[i]:
+        correct += 1 
     
-    print 100.0 * error / len(final_y)
+    print 100.0 * correct / len(final_y)
 
 def pi(n1, n2):
   if n1 != n2:
