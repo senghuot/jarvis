38c358e7c153814fc54108fabfa88b9b26700fb0
senghuot <senghuotlim@gmail.com>
2015-07-17 12:08:24 -0700

    working frontend

--- backend/server.py
+++ backend/server.py
@@ -15,17 +15,45 @@ def init():
 
 @app.route("/compute", methods=['GET', 'POST'])
 def compute():
-  # test posting
-  data = request.get_json()
-  print data['message']
-  
   # leave it here for testing only
-  init()
+  # init()
+
+  # parse our json object to a matrix array
+  customers = request.get_json()
+  data =  parse(customers['customers'])
 
   # compute the data and determine the classifier
-  data = [[147060 ,1 ,2 ,58824 ,13 , 4, 3, 10,  350000 , 20 , 0 ,-1 , -1  ,147160,  2]]
+  # data = [[147060 ,1 ,2 ,58824 ,13 , 4, 3, 10,  350000 , 20 , 0 ,-1 , -1  ,147160,  2]]
   res = classifier.compute(data)
   return jsonify(classifier=res)
+
+# this is a helper method to parse the data from json into a matrix to follow
+# the format and performance
+def parse(data):
+  res = []
+  for i in range(len(data)):
+    tmp_json = data[i]
+    tmp_array = []
+
+    tmp_array.append(tmp_json['amount'])
+    tmp_array.append(tmp_json['accountType'])
+    tmp_array.append(tmp_json['bookingType'])
+    tmp_array.append(tmp_json['expectedRevenue'])
+    tmp_array.append(tmp_json['numProducts'])
+    tmp_array.append(tmp_json['oppRecordType'])
+    tmp_array.append(tmp_json['forecastCategory'])
+    tmp_array.append(tmp_json['leadSource'])
+    tmp_array.append(tmp_json['annualRevenue'])
+    tmp_array.append(tmp_json['employees'])
+    tmp_array.append(tmp_json['hasCoreProducts'])
+    tmp_array.append(tmp_json['numLocations'])
+    tmp_array.append(tmp_json['numProducers'])
+    tmp_array.append(tmp_json['openOpp'])
+    tmp_array.append(tmp_json['openOppCount'])
+
+    res.append(tmp_array)
+
+  return res
 
 if __name__ == "__main__":
     app.run()  
