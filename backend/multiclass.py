9998742b3acc54369e054c0979dc640c8cc98150
senghuot <senghuotlim@gmail.com>
2015-07-23 22:20:23 -0700

    working prototype

--- backend/multiclass.py
+++ backend/multiclass.py
@@ -8,7 +8,7 @@ class Multiclass:
   def __init__(self):
     self.classifers = []
     self.rec_x      = []
-    self.ITERATIONS = 50
+    self.ITERATIONS = 182
     self.K          = 8
     self.train()
 
--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,16 @@ var keys = ["amount", "accountType", "bookingType", "expectedRevenue", "productC
     keys = keys.concat(["forecastCategory", "leadSource", "annualRevenue", "employeeCount"]);
     keys = keys.concat(["coreProduct", "locationCount", "producerCount", "oppRevenue", "oppCount"]);
 
+var map = {'accountType': {'-1':'Unknown', 1:'Customer', 2:'Former Customer', 3:'Government', 4:'Midwest', 5:'Online Customer', 6:'Partner', 7:'Prospect'}};
+    map['bookingType']      = {'-1':'Unknown', 1:'Addon', 2:'Cross-Sell', 3:'New Business'};
+    map['recordType']       = {'-1':'Unknown', 1:'Add-on', 2:'Administrative', 3:'Migration', 4:'Standard'};
+    map['forecastCategory'] = {'-1':'Unknown', 1:'Best Case', 2:'Commit', 3:'Pipeline', 4:'Ommited', 5:'Closed'};
+    map['coreProduct']      = {'-1':'Unknown', 0:'No', 1:'Yes'};
+    map['leadSource']       = {'-1':'Unknown', 1:'Agency Success Program', 2:'Ambest-List', 3:'Call-In', 4:'Client-Referall', 5:'Cold-Call', 6:'Contact List', 7:'Current Customer', 
+                        8:'Email', 9:'Demo', 10:'Employee Referal', 11:'Event', 12:'Web', 13:'Tradeshow', 14:'Seminar', 15:'External Referral', 16:'Jigsaw', 17:'Lead Pass', 
+                        18:'Marketing Campaign', 19:'Public Relations', 20:'Other'};
+    map['label'] = {1:'Won', 2:'Approved', 3:'Propose', 4:'Prospect', 5:'Qualify', 6:'Solution Developement', 7:'Whitespace', 8:'Closed'};
+
 demo.controller('IntroController', function ($scope, $http) {
   $scope.initBtnClicked = function(){
     // display loading screen
@@ -19,7 +29,6 @@ demo.controller('IntroController', function ($scope, $http) {
     $http.get('http://127.0.0.1:5000/init')
       .success(function(response){
         $('#loading').css('opacity', '0');
-        
         $('#go-down').toggle();
         audio.pause();
       }).error(function(error){
@@ -48,43 +57,37 @@ demo.controller('ComputeController', function($scope, $http) {
       });
   };
 
-  $scope.buildResult = function()
-  {
+  $scope.buildResult = function() {
     $scope.result = {};
-    var temp;
-    var name;
-    var val1;
-    var val2;
-    for (key in $scope.keys)
-    {
-      temp = [];
-      name = keys[key];
-      val1 = $scope.customer[name];
-      val2 = $scope.response.recommend[name];
-      temp.push(val1);
-      temp.push(val2);
-      $scope.result[name] = temp;
+    for (key in keys) {
+      var name            = keys[key];
+      var cus_val         = $scope.customer[name];
+      var rec_val         = $scope.response.recommend[name];
+      if (map[name] !== undefined) {
+        cus_val = map[name][cus_val];
+        rec_val = map[name][rec_val];
+      }
+      $scope.result[name] = [cus_val, rec_val];
     }
+    $scope.label = map['label'][$scope.response.label];
   };
 
   // convert sandbox string to useful JSON
   $scope.change = function() {
     var vals = $scope.sandbox.split("\t");
     var tmp = {};
-    for (var i=0; i<keys.length; i++) {
-      key = keys[i];
-      tmp[key] = vals[i]
+    for (var i = 0; i < keys.length; i++) {
+      key       = keys[i];
+      tmp[key]  = vals[i];
     }
     $scope.customer = tmp;
-    $scope.keys = keys;
   };
 });
-
 
 // initializing material ripple effect
 $(function() {
     $.material.init();
 });
 
-// initializing wow js
+// initializing wow.js
 new WOW().init();
